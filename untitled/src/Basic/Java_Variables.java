01600ba9e9f56d932380dfa33d2ff809d47ea610
NIpYo7 <nipunsuranimala40@gmail.com>
2026-02-24 00:23:21 +0530

    java variables

---
+++ untitled/src/Basic/Java_Variables.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class Java_Variables {
+    public static void main(String[] args){
+        int x=5;
+        int y=6;
+        System.out.println("The Sum is " + x+y);
+        System.out.println("The Sum is " + (x+y));
+
+
+        //Constant
+        final int mynum=15;
+        System.out.println("The Constant is " + mynum);
+
+        
+
+    }
+}
