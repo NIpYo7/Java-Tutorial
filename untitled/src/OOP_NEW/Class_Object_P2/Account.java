408cd3b0c95658f8687c7bf6768837bd06b1a28f
NIpYo7 <nipunsuranimala40@gmail.com>
2026-03-16 23:55:10 +0530

    OOP
    basic OOP part 03

---
+++ untitled/src/OOP_NEW/Class_Object_P2/Account.java
@@ -0,0 +1,21 @@
+package OOP_NEW.Class_Object_P2;
+
+public class Account {
+    int a,b;
+
+    public void setData(int a,int b){
+        a=a;
+        b=b;
+
+    }
+    public void showData(){
+        System.out.println("Value of A=" + a);
+        System.out.println("Value of B=" + b);
+    }
+
+    public static void main(String[] args) {
+        Account myAccount = new Account();
+        myAccount.setData(2,3);
+        myAccount.showData();
+    }
+}
